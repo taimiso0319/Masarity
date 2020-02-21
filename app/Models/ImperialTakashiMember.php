d21752e2d8ba5aacc08a86d7a9ab43d0f675a1f7
k-yasuda <yasuda.keisuke.1194@colopl.co.jp>
2020-02-21 12:52:09 +0900

    インペリアルたかしのメンバー用のモデル作成

---
+++ app/Models/ImperialTakashiMember.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ImperialTakashiMember extends Model
+{
+    protected $table = 'imperial_takashi_members_master';
+
+    public function get(){
+
+    }
+}
