fd20b778980bdaaa300fb307957e91207227b12e
k-yasuda <yasuda.keisuke.1194@colopl.co.jp>
2020-02-25 19:25:52 +0900

    TwitterAPIコントロール用のクラス作成

---
+++ app/Http/Controllers/TwitterController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TwitterController extends Controller
+{
+    private $profile_url_base = 'https://twitter.com/i/user/';
+}
