da5e49daf8deb7dbda58755ab3a01a6858f141cf
k-yasuda <yasuda.keisuke.1194@colopl.co.jp>
2020-02-28 14:31:11 +0900

    TwitterAPIを呼び出す際に、リクエストされたIDに応じてredisを利用するクラスを追加

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class TwitterController extends Controller
 {
-    private $profile_url_base = 'https://twitter.com/i/user/';
+    public function getUsersLookup(array $user_ids){
+        $hash = md5(implode($user_ids));
+        return \Cache::remember('TwitterController:getUsersLookup:'.$hash,6000, function() use ($user_ids) {
+            return \Twitter::getUsersLookup(['user_id'=>$user_ids]);
+        });
+    }
+
+    public  function show(){
+        return response()->json(\Twitter::getUsersLookup(['user_id'=>[153415601,null,76562029]]),200,[],JSON_PRETTY_PRINT);
+    }
 }
