54143380938edeb73955b4f230948526f6172945
k-yasuda <yasuda.keisuke.1194@colopl.co.jp>
2020-02-28 14:31:42 +0900

    Twitterの情報を付与して返せるAPIを実装

--- app/Http/Controllers/ImperialTakashiMembersController.php
+++ app/Http/Controllers/ImperialTakashiMembersController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Utils\Validation\ImperialTakashiMembersRequest;
+use App\Models\ImperialTakashiMember;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use phpDocumentor\Reflection\Types\Object_;
+use Thujohn\Twitter\Twitter;
 
 class ImperialTakashiMembersController extends Controller
 {
@@ -30,12 +35,64 @@ class ImperialTakashiMembersController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(ImperialTakashiMembersRequest $request)
     {
         //
+        $response = null;
+        $membersData = null;
+        $twitterData = null;
+        $steamData = null;
+        $vrchatData = null;
+        $twitterIds = [];
+        $steamIds = [];
+        // まだ使ってない。
+        $vrchatIds = [];
+
+        $imperialTakashiMemberModel = new ImperialTakashiMember();
+
+
+        // id指定があればそれだけ取り出す。
+        $id = $request->input('id');
+        if( $id === null){
+            $membersData = $imperialTakashiMemberModel->get();
+        }else{
+            $membersData = $imperialTakashiMemberModel->getRecords($id);
+        }
+        $response = $membersData;
+
+        // メンバーデータから各種IDを抽出しておく。(IDを配列で渡したい場合があるので。)
+        foreach ($membersData as $key => $membersDatum){
+            $twitterIds[$key] = $membersDatum->twitterId; // nullが入るとTwitterAPI側ではスキップされる
+            $steamIds[$key] = $membersDatum->steamId64;
+        }
+
+        // partにtwitterが含まれていればTwitterの情報を取得する。
+        if(in_array( 'twitter', $request->input('part'),true)){
+            $twitterController = new TwitterController();
+            $twitterData = $twitterController->getUsersLookup($twitterIds);
+        }
+        // partにsteamが含まれていればSteamの情報を取得する。
+
+        // partにvrchatが含まれていればVRChatの情報を取得する。
+
+        for ($i = 0, $iMax = count($response); $i < $iMax; $i++){
+            if($twitterData !== null){
+                if($membersData[$i]-> twitterId === null){
+                    // idが空だったらスキップ
+                    continue;
+                }
+                $response[$i]->twitter = new \stdClass();
+                $response[$i]->twitter->name = $twitterData[$i]->name;
+                $response[$i]->twitter->screen_name = $twitterData[$i]->screen_name;
+                $response[$i]->twitter->description = $twitterData[$i]->description;
+                $response[$i]->twitter->profile_background_image_url_https = $twitterData[$i]->profile_background_image_url_https;
+                $response[$i]->twitter->profile_image_url_https = $twitterData[$i]->profile_image_url_https;
+            }
+        }
+
+        return response()->json($response,200,[],JSON_PRETTY_PRINT);
     }
 
     /**
