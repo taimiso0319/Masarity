dfc7352489179eb6b62adb3cea80430efb46e3d9
k-yasuda <yasuda.keisuke.1194@colopl.co.jp>
2020-02-28 14:29:21 +0900

    不要な項目を削除

--- config/database.php
+++ config/database.php
@@ -127,7 +127,6 @@ return [
         ],
 
         'default' => [
-            'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', '6379'),
@@ -135,7 +134,6 @@ return [
         ],
 
         'cache' => [
-            'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', '6379'),
