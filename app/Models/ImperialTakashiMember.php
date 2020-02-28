12039ef91c01e0465089f5391c58783f99a18311
k-yasuda <yasuda.keisuke.1194@colopl.co.jp>
2020-02-28 14:30:31 +0900

    ImperialTakashiMemberモデルで一部データをredisでキャッシュするように変更

--- app/Models/ImperialTakashiMember.php
+++ app/Models/ImperialTakashiMember.php
@@ -3,12 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class ImperialTakashiMember extends Model
 {
     protected $table = 'imperial_takashi_members_master';
 
     public function get(){
-        return DB::table($this->table)->get();
+        return \Cache::remember('ImeprialTakashiMemberModel:get:',900 ,function(){ return DB::table($this->table)->get();});
+    }
+
+    public function getRecords($ids){
+        $query = DB::table($this->table);
+        foreach ($ids as $id) {
+            $query->orWhere('id', $id);
+        }
+        return $query->get();
     }
 }
