61aebab09d924fa6f488596481e2ae3fd0220b32
taimiso0319 <keisuke0319@gmail.com>
2020-03-01 20:27:16 +0900

    Jsonのレスポンスをエスケープしないようにしておく

---
+++ app/Http/Middleware/UnescapeJsonResponse.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class UnescapeJsonResponse
+{
+    /**
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle($request, \Closure $next)
+    {
+        $response = $next($request);
+
+        // JSON以外はそのまま
+        if (!$response instanceof JsonResponse) {
+            return $response;
+        }
+
+        // エンコードオプションを追加して設定し直す
+        $newEncodingOptions = $response->getEncodingOptions() | JSON_UNESCAPED_UNICODE;
+        $response->setEncodingOptions($newEncodingOptions);
+
+        return $response;
+    }
+}
