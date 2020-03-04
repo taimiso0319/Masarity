75fbb498250fa1841d6118e57ecc607575cea3b9
taimiso0319 <keisuke0319@gmail.com>
2020-03-05 00:34:13 +0900

    axiosのcreateで利用していたURLにドメインがあったが、ローカル開発の問題がありそうだったので修正した。

--- resources/js/utils/repositories/repository.js
+++ resources/js/utils/repositories/repository.js
@@ -1,6 +1,5 @@
 import axios from 'axios/index'
 
-const baseDomain = process.env.MIX_API_BASE_DOMAIN
-const baseURL    = `${baseDomain}/api/v1`
+const baseURL    = `/api/v1`
 
 export default axios.create({ baseURL: baseURL })
